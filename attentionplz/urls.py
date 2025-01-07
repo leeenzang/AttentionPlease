cb92908fac90f904fd2ff8060ce47e73847909d7
leeeunjin <enzn@hufs.ac.kr>
2025-01-07 10:57:28 +0900

    FEAT: 비디오 처리 도구 추가

--- attentionplz/urls.py
+++ attentionplz/urls.py
@@ -38,6 +38,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # users 앱 연결
+    path('upload/', include('upload.urls')),  
+    path('feedback/', include('feedback.urls')),  
+    
     # Swagger 문서 경로
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
